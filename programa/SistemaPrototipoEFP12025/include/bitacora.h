606664bb5df99e1df13b4f2d894fd10607899d68
steven vasquez <svasquezc12@miumg.edu.gt>
2025-05-28 10:31:03 -0600

    sistema prototipo

---
+++ programa/SistemaPrototipoEFP12025/include/bitacora.h
@@ -0,0 +1,8 @@
+#ifndef BITACORA_H
+#define BITACORA_H
+
+#include <string>
+
+void registrarEvento(const std::string& usuario, const std::string& evento);
+
+#endif
---
+++ programa/SistemaPrototipoEFP12025/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include "Login.h"
+#include "usuario.h"
+#include "bitacora.h"
+
+using namespace std;
+
+
+string usuarioActual; // Para guardar el nombre del usuario bitacora
+void mostrarMenuPrincipal();
+
+int main() {
+    int opcion;
+    bool accesoPermitido = false;
+
+    do {
+
+        system ("cls");
+        cout << "\n--- Bienvenido al Sistema ---\n";
+        cout << "1. Iniciar Sesion\n";
+        cout << "2. Registrarse\n";
+        cout << "3. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+        cin.ignore(); // Para evitar errores con getline
+
+        switch (opcion) {
+            case 1:
+                registrarEvento(usuarioActual, "SE INICIO SESION 5001");
+                accesoPermitido = iniciarSesion(usuarioActual);
+                break;
+            case 2:
+                registrarEvento(usuarioActual, "SE REGISTRO UN NUEVO USUARIO 5001");
+                registrarUsuario();
+                break;
+            case 3:
+                cout << "\n\nSaliendo del sistema...\n";
+                return 0;
+            default:
+                cout << "Opción invalida.\n";
+        }
+    } while (!accesoPermitido);
+
+    mostrarMenuPrincipal();
+     registrarEvento(usuarioActual, "Cerró sesión desde menú principal");
+    return 0;
+}
+
+
+
+
+void mostrarMenuPrincipal() {
+    int opcion;
+    do {
+        system ("cls");
+        cout << "\n\t\t\t|------------------------ \n" ;
+        cout << "\t\t\t|     MENU PRINCIPAL     | \n";
+        cout << "\t\t\t|------------------------- \n" ;
+        cout << "\t\t\t|1. Catalogos\n";
+        cout << "\t\t\t|2. Procesos\n";
+        cout << "\t\t\t|3. Salir del sistema\n";
+        cout << "\t\t\t|Seleccione una opcion: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                registrarEvento(usuarioActual, "Salio del menu general 5002 ");
+                cout << "Cerrando sesion...\n";
+                break;
+            default:
+                cout << "Opcion no valida.\n";
+        }
+    } while (opcion != 3);
+}
+
